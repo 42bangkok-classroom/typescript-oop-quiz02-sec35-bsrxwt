426374f36252be2b2d0b7e85d89f03edd1eb961b
bsrxwt <sorawit281249@gmail.com>
2026-01-16 10:09:42 +0700

    p03-1

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,1 +1,40 @@
-export function mapPostWithCommentCount() {}
+import axios from "axios";
+interface Posts{
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+interface Comments {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
+interface reComments{
+    postId: number;
+    title: string;
+    totalComments: number;
+}
+export const mapPostWithCommentCount = async(): Promise<reComments[]> => {
+    try{
+        const url1 = "";
+        const url2 = "";
+        const resPosts = await axios.get<Posts[]>(url1);
+        const resComments = await axios.get<Comments[]>(url2);
+        const posts = resPosts.data;
+        const comments = resComments.data;
+        const result = posts.map((post)=>{
+        const count = comments.filter(comment => comment.postId === post.id).length;
+            return{
+                postId: post.id,
+                title: post.title,
+                totalComments: count,
+            }
+        });
+        return result;
+    }catch(error){
+        return [];
+    }
+}
