fcb10ef3d5b80ffc99bde74b465e5cee95354c5d
bsrxwt <sorawit281249@gmail.com>
2026-01-16 10:41:19 +0700

    finalboss

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,25 @@
-export function countCommentsByPost() {}
+import axios from "axios";
+interface Comment{
+  postId: number;
+  id:number;
+  name: string;
+  email: string;
+  body: string;
+}
+interface CommentCounts {
+  [key: number]:number;
+}
+export const countCommentsByPost = async(): Promise<CommentCounts> => {
+    try {
+        const url = "https://jsonplaceholder.typicode.com/comments";
+        const response = await axios.get<Comment[]>(url);
+        const comments = response.data;
+        const result = comments.reduce((acc, comment) => {      
+        acc[comment.postId] = (acc[comment.postId] || 0) + 1;
+          return acc;
+        }, {} as CommentCounts);
+        return result;
+    } catch (error) {
+        return {};
+    }
+};
