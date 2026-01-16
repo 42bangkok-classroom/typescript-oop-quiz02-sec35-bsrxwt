b5d15a8c3c9b60ae8da028b9c123da3214e18943
bsrxwt <sorawit281249@gmail.com>
2026-01-16 09:50:26 +0700

    p02-1

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,25 @@
-export function getPostsByUser() {}
+import axios from "axios";
+interface Posts{
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+interface UserSummary {
+  id: number;
+  title: string;
+}
+export const getPostsByUser = async(userId: number): Promise<UserSummary[]> =>{
+    try{
+        const url = "https://jsonplaceholder.typicode.com/posts";
+        const resPost = await axios.get<Posts[]>(url);
+        const posts = resPost.data;
+        const result = posts.map((post)=>({
+            id: post.id,
+            title: post.title,
+        }));
+        return result;
+    }catch(error){
+        return [];
+    }
+}
