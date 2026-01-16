2d5e124bdcfb36a63e2238faeb2c3dfe3605f75d
bsrxwt <sorawit281249@gmail.com>
2026-01-16 10:20:25 +0700

    p05-1

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,2 +1,22 @@
-export function safeFetchComment() {}
+import axios from "axios";
+interface Comments {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
+interface reComment{
+    id: number;
+    body: string;
+}
+export const safeFetchComment = async(commentId:number): Promise<reComment | null> => {
 
+    try{
+        const resComment = await axios.get<Comments>("https://jsonplaceholder.typicode.com/comments/{id}");
+        const {id,body} = resComment.data;
+        return {id,body};
+    }catch(error){
+    return null;
+    }
+}
