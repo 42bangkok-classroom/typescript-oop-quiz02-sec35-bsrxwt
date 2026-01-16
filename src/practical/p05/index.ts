23d4570965a173f1705187103987db60cf12b6a2
bsrxwt <sorawit281249@gmail.com>
2026-01-16 11:10:49 +0700

    maitest

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -13,7 +13,7 @@ interface reComment{
 export const safeFetchComment = async(commentId:number): Promise<reComment | null> => {
 
     try{
-        const resComment = await axios.get<Comments>(``);
+        const resComment = await axios.get<Comments>(`https://jsonplaceholder.typicode.com/comments/${commentId}`);
         const {id,body} = resComment.data;
         return {id,body};
     }catch(error){
